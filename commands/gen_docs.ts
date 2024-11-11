8f7bcfdf87d2d7c8ac8d292ebd038d39f57012d4
LE VAN THANH <zero.lethanh@gmail.com>
2024-11-11 11:48:05 +0700

    init

---
+++ commands/gen_docs.ts
@@ -0,0 +1,5 @@
+function gen_docs() {
+    console.log("this is gen:docs cmd");
+}
+
+export { gen_docs };
---
+++ main.ts
@@ -0,0 +1,16 @@
+import {program} from "npm:commander";
+import {gen_docs} from "./commands/gen_docs.ts";
+
+program
+    .command("gen:docs")
+    .description("generate openapi documents")
+    .action(gen_docs);
+
+// export function add(a: number, b: number): number {
+//     return a + b;
+// }
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+    program.parse();
+}
