a1a75d2ed1894ed364ca8a6d4984454228d352b4
LE VAN THANH <zero.lethanh@gmail.com>
2025-06-06 21:30:59 +0700

    v3.5

--- src/commands/make_schema.ts
+++ src/commands/make_schema.ts
@@ -1,10 +1,7 @@
-import { z } from "zod";
-import { GlobalCommandInputSchema } from "@/types/GlobalCommandInputSchema";
-import {
-  getCommandInputDeclarationCode,
-  getParsedData,
-} from "@/util/commandParser";
-import { readTemplate } from "@/commands/index";
+import {z} from "zod";
+import {GlobalCommandInputSchema} from "@/types/GlobalCommandInputSchema";
+import {getCommandInputDeclarationCode, getParsedData,} from "@/util/commandParser";
+import {readTemplate} from "@/commands/index";
 import {camelCase} from "lodash";
 import {makeFile, srcPath} from "@/util/pathUtils";
 import {logDone} from "@/util/logger";
@@ -25,7 +22,7 @@ export function make_schema() {
   const code = get_code(data);
   // implementations
   const schemaName = camelCase(data.name.replace("/", "_") );
-  const flowWriteTo = srcPath('flows', schemaName, SCHEMA_DIR, `${schemaName}.ts`)
+  const flowWriteTo = srcPath(SCHEMA_DIR, `${schemaName}.ts`)
   const doneWriteFlow = makeFile(flowWriteTo, code, data.force)
   if (doneWriteFlow) {
     logDone(flowWriteTo)
